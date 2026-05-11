5157011aa81a85d65b39ea1148ebde9c1da11a59
Alexandr <kodak333vas@gmail.com>
2026-05-12 02:46:45 +0300

    erwrwe

--- SpecialHeader.js
+++ SpecialHeader.js
@@ -85,7 +85,7 @@ class SpecialHeader extends HTMLElement {
                 <svg viewBox="0 0 24 24"><path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/></svg>
             </div>
             <div class="contact-info" style="text-align: left;">
-                <a href="tel:79999030408" class="main-text">+7 (999) 903-04-08</a>
+                <a href="tel:+79999030408" class="main-text">+7 (999) 903-04-08</a>
                 <div class="sub-text">Ежедневно с 10:00 до 20:00</div>
             </div>
         </div>
